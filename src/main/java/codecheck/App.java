fd21830e17e5ab1a0bbf6a8836664eed89d20e9e
tie209192 <tie209192@S1706C10854-T1.tisnt.tis.co.jp>
2018-06-14 19:39:06 +0900

    aaaaa

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,7 +3,7 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 
-
+try {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String request = args[i];
 			int kazu = Integer.parseInt(request);
@@ -21,6 +21,9 @@ public class App {
 				System.out.println("invalid");
 			}
 		}
+	}catch(NumberFormatException e) {
+		System.out.println("invalid");
 	}
+
 }
 
