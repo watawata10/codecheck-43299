dc6429e3523539b3264daaab2336a07cf6503978
tie209192 <tie209192@S1706C10854-T1.tisnt.tis.co.jp>
2018-06-14 19:57:34 +0900

    aaaa

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -3,27 +3,32 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 
-try {
+
 		for (int i = 0, l = args.length; i < l; i++) {
 			String request = args[i];
-			int kazu = Integer.parseInt(request);
-			int waru3noamari = kazu % 3;
-
-			if (waru3noamari == 0 && kazu <= 1000 && kazu > 0 && request.contains("3")) {
-				System.out.println("dumb");
-			}else if (waru3noamari == 0 && kazu <= 1000 && kazu > 0) {
-					System.out.println("idiot");
-			}else if (request.contains("3") && kazu <= 1000 && kazu > 0) {
-					System.out.println("stupid");
-			}else if ( kazu <= 1000 && kazu > 0) {
-				System.out.println("smart");
+			if (request.getBytes().length ==  request.length()){
+			try {
+				int kazu = Integer.parseInt(request);
+				int waru3noamari = kazu % 3;
+					if (waru3noamari == 0 && kazu <= 1000 && kazu > 0 && request.contains("3")) {
+						System.out.println("dumb");
+					}else if (waru3noamari == 0 && kazu <= 1000 && kazu > 0) {
+						System.out.println("idiot");
+					}else if (request.contains("3") && kazu <= 1000 && kazu > 0) {
+						System.out.println("stupid");
+					}else if ( kazu <= 1000 && kazu > 0) {
+						System.out.println("smart");
+					}else {
+						System.out.println("invalid");
+					}
+				}catch(NumberFormatException e) {
+					System.out.println("invalid");
+				}
 			}else {
 				System.out.println("invalid");
+
 			}
 		}
-	}catch(NumberFormatException e) {
-		System.out.println("invalid");
 	}
-
 }
 
